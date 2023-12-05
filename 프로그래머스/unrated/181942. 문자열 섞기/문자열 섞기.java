4a6478ce8964675ef3d32c1cbe2da5c0b6479012
sue <150881952+ddu2ni@users.noreply.github.com>
2023-12-05 13:29:52 +0900

    Update 문자열 섞기.java

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -3,7 +3,9 @@ class Solution {
         String answer = "";
         
         for(int i=0;i<str1.length();i++){
-           answer += str1.charAt(i)+""+str2.charAt(i); //
+           answer += str1.charAt(i)+""+str2.charAt(i);
+         // 문자와 +(문자: 연산자)가 합쳐지면 ASCII Code값으로 인식해서 숫자를 출력
+        //앞에다 ""를 넣어서 실행하면 "" + charAt() => 문자열 + str2.charAt() => 문자열
         }
         
             return answer;
