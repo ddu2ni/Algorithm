88e51325f08449add3ae6bd0a8147097631424cc
sue <150881952+ddu2ni@users.noreply.github.com>
2023-12-05 13:34:19 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.23 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
-        for(int i=0;i<str1.length();i++){
-           answer += str1.charAt(i)+""+str2.charAt(i);
-         // 문자와 +(문자: 연산자)가 합쳐지면 ASCII Code값으로 인식해서 숫자를 출력
-        //앞에다 ""를 넣어서 실행하면 "" + charAt() => 문자열 + str2.charAt() => 문자열
+        for (int i = 0; i < str1.length(); i++) {
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
+            System.out.print(answer);
         }
         
-            return answer;
+        return answer.toString();
     }
-
 }
