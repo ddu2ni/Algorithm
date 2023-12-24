a70184782adc7bf1273cdd7e62a62b5af6949154
sue <150881952+ddu2ni@users.noreply.github.com>
2023-12-24 15:53:41 +0900

    [level unrated] Title: 배열 만들기 4, Time: 5.35 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,21 +1,22 @@
+package soultion;
+
 import java.util.ArrayList;
 
-public class Solution {
-    public ArrayList<Integer> solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
-        int i = 0;
+class Solution1 {
+	public ArrayList<Integer> solution(int[] arr) {
+		
+        ArrayList<Integer> stk = new ArrayList();
+        
+        for(int i=0;i<arr.length;i++) {
 
-        while (i < arr.length) {
-            if (stk.isEmpty()) {
-                stk.add(arr[i]);
-                i++;
-            } else if (stk.get(stk.size() - 1) < arr[i]){
-                stk.add(arr[i]);
-                i++;
-            } else {
-                stk.remove(stk.size() - 1);
-            }
-        }
+           	   if(stk.isEmpty()) {
+           		  stk.add(arr[i]);
+           	   }else if(stk.get(stk.size()-1)<arr[i]) {
+           		stk.add(arr[i]);
+           	   }else {
+           		   stk.remove(stk.size()-1);
+           	   }
+       	   }
         return stk;
     }
 }
